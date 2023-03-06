d6fdd405db75d55b47a73e22444952a7d02182c2
shm <ajdwk91@naver.com>
2023-03-06 11:30:31 +0900

    "src 폴더 정리. 기본 파일 삭제. css 폴더 추가."

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './css/index.css';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Login />
+    {/*{status = 1 ? <Login /> : null} 값에 따라서 화면 바뀌게 설정하기*/}
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
