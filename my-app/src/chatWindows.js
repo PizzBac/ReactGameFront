3ef32d45c52020ad43cd1725d16bd23a73d05af2
SHM <ajdwk91@naver.com>
2023-03-07 02:03:49 +0900

    채팅창 수정

--- my-app/src/chatWindows.js
+++ my-app/src/chatWindows.js
@@ -1,69 +1,70 @@
-// 소켓 안 쓰고 그냥 useState로 입출력하는 코드
 import React from 'react';
 import styles from "./css/TestBoard.module.css";
+import { FaPaperPlane } from "react-icons/fa";
+
+// 소켓 안 쓰고 그냥 useState로 입출력하는 코드
 
 function Chat() {
+    // const [name, setName] = React.useState("");
     const [message, setMessage] = React.useState("");
-    const [name, setName] = React.useState("");
     const [chatLog, setChatLog] = React.useState([]);
-
-
-
 
     // 새로운 메시지가 추가될 때마다 자동으로 스크롤을 아래쪽으로 이동
     const messagesEndRef = React.useRef(null); // useRef를 이용하여 messagesEndRef 변수 생성
-      // useEffect 등록
-  React.useEffect(() => {
-    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [chatLog]);
+    React.useEffect(() => { // useEffect 등록
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }, [chatLog]);
 
+    // const handleChangeName = (event) => {
+    //     setName(event.target.value);
+    // };
 
     const handleChangeMessage = (event) => {
         setMessage(event.target.value);
     };
 
-    const handleChangeName = (event) => {
-        setName(event.target.value);
-    };
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        const newMessage = { name, message };
+        const newMessage = { message };
         setChatLog([...chatLog, newMessage]);
-        setName("");
+        // setName("");
         setMessage("");
     };
 
     return (
-        <div>
+        <div className={styles.chatWrapper}>
             {/* 채팅 목록 */}
-            <div className={styles.chatcontainer}>
-                {chatLog.map((item, index) => (
-                    <div className="text" key={index}>
-                        <strong>{item.name}:</strong> {item.message}
-                    </div>
-                ))}
-                <div ref={messagesEndRef} />
-                {/*<div ref={messagesEndRef}></div>*/} {/* ref 속성을 이용하여 messagesEndRef 변수와 div 요소를 연결 */}
-            </div>
-
+            <div className={styles.chatcontainer} >
+                {
+                    chatLog.map((item, index) => (
+                        <div className="text" key={index}>
+                            {/* <strong>{item.name}:</strong> {item.message} */}
+                            <strong>Guest:</strong> {item.message}
+                        </div>
+                    ))
+                }
+                < div ref={messagesEndRef} />
+                {/* ref 속성을 이용하여 messagesEndRef 변수와 div 요소를 연결 */}
+            </div >
             {/* 메시지 입력 폼 */}
             <form onSubmit={handleSubmit}>
-                <label>
+                {/* <label>
                     이름:
                     <input type="text" value={name} onChange={handleChangeName} />
                 </label>
-                <br />
-                <label>
+                <br /> */}
+                <label className={styles.messageInput}>
                     메시지:
                     <input
                         type="text"
                         value={message}
                         onChange={handleChangeMessage}
                     />
+                    {/* <button className={styles.submitBtn} type="submit"><i class="fa fa-paper-plane"></i></button> */}
+                    <span className={styles.submitBtn} onClick={handleSubmit}>
+                        <FaPaperPlane />
+                    </span>
                 </label>
-                <br />
-                <button type="submit">보내기</button>
             </form>
         </div>
     );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,13 +4,12 @@ import App from './Game';
 import Game from "./Game";
 import Login from './Login';
 import SignUp from './SignUp';
-import Main from './Main';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    {/* <Game /> */}
+    <Game />
     {/* <SignUp /> */}
-    <Main />
     {/* <Login /> */}
   </div>
 );
