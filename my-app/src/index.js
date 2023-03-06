9e0f2222b7e966c9b2ea178b5812b44645390568
sdakfjasldkfjsldkajflksadjfklas <thghkstk12342@naver.com>
2023-03-06 15:18:39 +0900

    signup 꾸밈

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Game from "./Game";
+import SignUp from './SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <Game />
+    {/* <Game /> */}
+    <SignUp />
   </div>
 );
