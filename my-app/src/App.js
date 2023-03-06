34dd1d8f1caf90a7424fdacc387d524722c62ef0
PizzBac <joshhartnett946@gmail.com>
2023-03-06 12:39:31 +0900

    0306AM push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
+import ChatWindows from "./chatWindows";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <img src={ require('./images/1.png') } alt="hi" />
-      </header>
+    
+    <div className="page">
+
+
+
+
+      <div className="GameBoard">
+        <div className="console">
+            
+        </div>
+
+
+        <div className="player player1">
+            <div className="cardSet">
+              <p className="playerId"># Player 1</p>
+              <img className="card cardFirst" src={ require('./images/assassin.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/duke.png') } alt="duke" />
+            </div>
+        </div>
+
+        <div className="player player2">
+            <div className="cardSet">
+              <p className="playerId"># Player 2</p>
+              <img className="card cardFirst" src={ require('./images/assassin.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/contessa.png') } alt="duke" />
+            </div>
+        </div>
+
+        <div className="player player3">
+            <div className="cardSet">
+              <p className="playerId"># Player 3</p>
+              <img className="card cardFirst" src={ require('./images/captin.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/contessa.png') } alt="duke" />
+            </div>
+        </div>
+
+        <div className="player player4">
+            <div className="cardSet">
+              <p className="playerId"># Player 4</p>
+              <img className="card cardFirst" src={ require('./images/contessa.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/contessa.png') } alt="duke" />
+            </div>
+        </div>
+
+        <div className="player player5">
+            <div className="cardSet">
+              <p className="playerId"># Player 5</p>
+              <img className="card cardFirst" src={ require('./images/assassin.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/duke.png') } alt="duke" />
+            </div>
+        </div>
+
+        <div className="player player6">
+            <div className="cardSet">
+              <p className="playerId"># Player 6</p>
+              <img className="card cardFirst" src={ require('./images/captin.png') } alt="assassin" />
+              <img className="card cardSecond" src={ require('./images/captin.png') } alt="duke" />
+            </div>
+        </div>
+
+
+
+      </div>
+
+      <div className="sideSection">
+        <div className="img hintBox">
+          <span className="hintTitle">게임 힌트</span>
+        </div>
+        <ChatWindows />
+      </div>
+
+
+
+
     </div>
   );
 }
---
+++ my-app/src/chatWindows.js
@@ -0,0 +1,72 @@
+// 소켓 안 쓰고 그냥 useState로 입출력하는 코드
+import React from 'react';
+import styles from "./TestBoard.module.css";
+
+function Chat() {
+    const [message, setMessage] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [chatLog, setChatLog] = React.useState([]);
+
+
+
+
+    // 새로운 메시지가 추가될 때마다 자동으로 스크롤을 아래쪽으로 이동
+    const messagesEndRef = React.useRef(null); // useRef를 이용하여 messagesEndRef 변수 생성
+      // useEffect 등록
+  React.useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  }, [chatLog]);
+
+
+    const handleChangeMessage = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handleChangeName = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newMessage = { name, message };
+        setChatLog([...chatLog, newMessage]);
+        setName("");
+        setMessage("");
+    };
+
+    return (
+        <div>
+            {/* 채팅 목록 */}
+            <div className={styles.chatcontainer}>
+                {chatLog.map((item, index) => (
+                    <div key={index}>
+                        <strong>{item.name}:</strong> {item.message}
+                    </div>
+                ))}
+                <div ref={messagesEndRef} />
+                {/*<div ref={messagesEndRef}></div>*/} {/* ref 속성을 이용하여 messagesEndRef 변수와 div 요소를 연결 */}
+            </div>
+
+            {/* 메시지 입력 폼 */}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    이름:
+                    <input type="text" value={name} onChange={handleChangeName} />
+                </label>
+                <br />
+                <label>
+                    메시지:
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={handleChangeMessage}
+                    />
+                </label>
+                <br />
+                <button type="submit">보내기</button>
+            </form>
+        </div>
+    );
+}
+
+export default Chat;
