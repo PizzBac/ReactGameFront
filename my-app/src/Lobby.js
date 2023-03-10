9b9c248392ac83bbba3ae256eec7d46e65931032
shm <ajdwk91@naver.com>
2023-03-10 13:28:38 +0900

    자잘한 버그 수정

--- my-app/src/Lobby.js
+++ my-app/src/Lobby.js
@@ -2,6 +2,7 @@ import React from "react";
 import './css/Lobby.css';
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { PieChart } from 'react-minimal-pie-chart';
 
 function Lobby() {
     const navigate = useNavigate();
@@ -30,23 +31,18 @@ function Lobby() {
     }
 
     return (
-
         <div className="BackGround">
             <div>
                 <h1 className="LobbyTitle">Welcome to the Coup!</h1>
                 <div>
-
                     <h1 className="imgdoor"></h1>
-
                     <h3 className="SubTitle1">원하는 인원수를 선택하세요
-
-
                         <select
                             value={howManyPlayer} // 현재 선택한 값을 표시
                             onChange={(e) => setHowManyPlayer(parseInt(e.target.value))} // 선택한 값을 저장
                         >
                             {[...Array(5)].map((_, i) => (
-                                <option value={i + 2}>{i + 2}인방</option>
+                                <option key={i+2} value={i + 2}>{i + 2}인방</option>
                             ))}
                         </select></h3><br />
                     <h3 className="SubTitle2">자신의 위치를 선택하세요
@@ -55,20 +51,8 @@ function Lobby() {
                             onChange={(ev) => setLoginPlayerNum(parseInt(ev.target.value))}
                         >
                             {[...Array(6)].map((_, i) => (
-                                <option value={i + 1}>{i + 1}번</option>
+                                <option key={i+1} value={i + 1}>{i + 1}번</option>
                             ))}
-
-                        </select></h3>
-
-                    <h3 className="SubTitle3">대충 이상한거(나도 안정함)
-                        <select
-                            value={loginPlayerNum}
-                            onChange={(ev) => setLoginPlayerNum(parseInt(ev.target.value))}
-                        >
-                            {[...Array(6)].map((_, i) => (
-                                <option value={i + 1}>{i + 1}번</option>
-                            ))}
-
                         </select></h3>
                     <button className="GameStart" onClick={GameStart}>Game Start</button>
                     <button className="GoLogin" onClick={LoginBtn}>로그아웃</button>
