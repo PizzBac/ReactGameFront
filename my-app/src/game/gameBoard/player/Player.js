050edd046eea2122642ab4158331b190e1dcf935
shm <ajdwk91@naver.com>
2023-03-13 16:22:21 +0900

    Turn.js - 플레이어 차례 구현

--- my-app/src/game/gameBoard/player/Player.js
+++ my-app/src/game/gameBoard/player/Player.js
@@ -53,7 +53,7 @@ function Player(props) {
 
   return (
     <div>
-      <Turn players={players} howManyPlayer={players.length} />
+      {/* <Turn players={players} howManyPlayer={players.length} /> */}
       {players.map((player) => (
         <div key={player.player.id} className={`player player${player.player.id} ${activate === true ? "active" : ""}`}>
           <div className={`cardSet ${activate === true ? "active" : ""}`}>
